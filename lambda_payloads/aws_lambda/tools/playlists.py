b5e3959317de64dea5184c562fbb044e03560e91
Maheen <164035371+maheenrathod@users.noreply.github.com>
2024-05-22 11:24:11 -0400

    Create playlists.py

---
+++ lambda_payloads/aws_lambda/tools/playlists.py
@@ -0,0 +1,17 @@
+import spotipy
+import os
+from spotipy.oauth2 import SpotifyClientCredentials
+
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+    client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET')
+))
+
+def get_artists_from_playlist(playlist_uri):
+    artists = {}
+    playlist_tracks = spotify.playlist_tracks(playlist_id=playlist_uri)
+    for song in playlist_tracks['items']:
+        if song['track']:
+            print(song['track']['artists'][0]['name'])
+            artists[song['track']['artists'][0]['uri']] = song['track']['artists'][0]['name']
+    return artists
